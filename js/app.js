9fa42fd7cefa46c584e54acd022904b1329dfadc
kojiadrianojr <rolandokoji.adriano@bicol-u.edu.ph>
2019-06-16 22:48:27 +0800

    you can do this!!!

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const apis = {
     characters,
     location,
-    episodes,
+    episode,
     quotes,
 };
 
+var ele;
+var epiChar = [];
 var randomN;
 var currPage;
 
@@ -16,7 +18,78 @@ const randomChar = () => {
 
 $(document).ready(function() {
     $('#more-char').trigger('click')
-})
+
+    const dispSeason = (() => {
+        var $seasonSelect = $('#sel-season');
+        maxPage.then(res => {
+            for (i = 1; i <= res; i++) {
+                $seasonSelect.append(
+                    `
+                     <option value="${i}">Season: ${i}</option>
+                    `).trigger('change');
+            }
+        })
+    })();
+
+
+
+    $('#sel-season').change(function() {
+        let season = $('#sel-season').val()
+        episode.getPage(season).then(season => {
+            var episodes;
+            $('#sel-episode').html(season.results.map(epis => {
+                    episodes = epis.name
+                    return `<option value="${episodes}">${episodes}</option>`
+                }).join(''))
+                .trigger('change')
+
+        })
+    })
+    $('#sel-episode').change(function() {
+            let episodes = $('#sel-episode').val()
+            var data = [];
+            episode.get(episodes)
+                .then(epi => {
+                    data.push(...epi.results)
+                    return data
+                })
+                .then(res => data[0].characters)
+                .then(char => {
+                    $('.char-box').html(char.map(url => {
+                        return renderChar(url)
+                            .then(char => {
+                                img = char.image;
+                                name = char.name;
+                                status = char.status;
+                                species = char.species;
+                                type = char.type;
+                                origin = char.origin.name;
+                                $('.char-box').append(`
+                                <div class="char-mini">
+                                <img src="${img}">
+                                <h3>Information</h3>
+                                <p>
+                                Name: ${name} <br>
+                                Status: ${status} <br>
+                                Species: ${species} <br>
+                                Type: ${type} <br>
+                                Origin: ${origin} <br>
+                                </p>
+                            </div>
+                                `)
+                            })
+                    }))
+                })
+
+
+
+
+        })
+        //.trigger('change')
+
+});
+
+
 
 $('#more-char').click(function(e) {
     const getRandomChar = (() => {
@@ -26,8 +99,8 @@ $('#more-char').click(function(e) {
         characters.get(randomN)
             .then(res => {
                 $right.html(`
-                <img id="char-info" src="${res.image}">
-                <p id="char-info" class="orbitron"><strong>${res.name}</strong><br><span>Click for info!</span></p>
+                <img id="char-info" class="hvr-float-shadow" src="${res.image}">
+                <p id="char-info" class="orbitron hvr-bubble-float-left"><strong>${res.name}</strong><br><span>Click for info!</span></p>
                 
                 `)
                 $infoHere.html(`
@@ -48,9 +121,18 @@ $('#more-char').click(function(e) {
     })();
 })
 
-const renderChar = () => {
-        return characters.all()
-            .then(res => console.log(res))
+const maxPage = (() => {
+    return episode.getInfo()
+        .then(res => res)
+        .then(arr => arr.info)
+        .then(info => info.pages)
+})();
+
+
+
+const renderChar = (url) => {
+        return fetch(url)
+            .then(res => res.json())
     }
     /*
     const displayAll = (() => {
@@ -60,8 +142,8 @@ const renderChar = () => {
                     characters.getPage(i)
                         .then(page => page.results)
                         .then(res => res.map(char => {
-                            $('.char-cont').append(`
-                            <div class="char-box-hidden">
+                            $('.char-here').append(`
+                            <div class="char-box">
                                 <h1>${char.name}</h1>
                             </div>
                                 `)
--- js/episode.js
+++ js/episode.js
@@ -7,11 +7,20 @@ const episode = (() => {
     }
 
     function get(epi) {
-        return rAmAPI.get(`episode/${epi}`);
+        return rAmAPI.get(`episode/?name=${epi}`);
     }
 
+    function getPage(page) {
+        return rAmAPI.get(`episode/?page=${page}`);
+    }
+
+    function getInfo() {
+        return rAmAPI.get(`episode`);
+    }
     return {
         all,
         get,
+        getPage,
+        getInfo,
     }
 })();
